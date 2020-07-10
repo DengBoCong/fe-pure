5fc8d7e431b1f5418e8bd10174359bccc6b8b053
DBC <1210212670@qq.com>
2020-07-10 18:14:48 +0800

    '完成吐槽说明基础结构'

--- src/views/about/Explain.vue
+++ src/views/about/Explain.vue
@@ -1,26 +1,49 @@
 <template>
-  <div>新建</div>
+  <el-container>
+    <el-header>
+      <Header></Header>
+    </el-header>
+    <el-container>
+      <el-main :style="height" class="hiddenScrollbar">
+        <Markdown v-bind:content="content"></Markdown>
+      </el-main>
+      <el-aside width="500px" :style="height" class="hiddenScrollbar">
+        <QuickSearch></QuickSearch>
+      </el-aside>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
+import Markdown from 'components/Markdown';
+import Header from 'components/Header'
+import QuickSearch from 'components/context/QuickSearch'
+
 export default {
   name: 'Explain',
   components: {
-    //
+    Header,
+    QuickSearch,
+    Markdown,
   },
   methods: {
     //
   },
   data() {
     return {
-      //
+      content: '<p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p><p>sd</p>',
+      screenWidth: 0,
+      screenHeight:0,
     }
   },
   computed: {
-    //
+    height(){
+      return `height:${this.screenHeight-61}px`;
+    },
   },
   mounted() {
-    //
+    this.screenWidth = document.body.clientWidth;
+    this.screenHeight = document.body.clientHeight;
   }
 }
 </script>
