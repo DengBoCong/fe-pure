a49477ac3fa63115816a109f049fc3f4d807d2fc
DBC <1210212670@qq.com>
2020-08-03 23:44:14 +0800

    '更新资源列表概览结构'

--- src/views/technique/Main.vue
+++ src/views/technique/Main.vue
@@ -1,7 +1,23 @@
 <template>
-  <div>
-    sdf
-  </div>
+  <el-container>
+    <el-aside width="40%">
+      <el-card>
+        <el-carousel trigger="click" height="200px">
+          <el-carousel-item v-for="item in 4" :key="item">
+            <h3 class="small">{{ item }}</h3>
+          </el-carousel-item>
+        </el-carousel>
+      </el-card>
+      <el-card>
+        <el-carousel height="200px" direction="vertical" :autoplay="false">
+          <el-carousel-item v-for="item in 3" :key="item">
+            <h3 class="medium">{{ item }}</h3>
+          </el-carousel-item>
+        </el-carousel>
+      </el-card>
+    </el-aside>
+    <el-main>Main</el-main>
+  </el-container>
 </template>
 
 <script>
@@ -28,3 +44,21 @@ export default {
   }
 }
 </script>
+
+<style lang="less">
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 200px;
+    margin: 0;
+  }
+
+  .el-carousel__item:nth-child(2n) {
+     background-color: #99a9bf;
+  }
+  
+  .el-carousel__item:nth-child(2n+1) {
+     background-color: #d3dce6;
+  }
+</style>
